86a6a8505eb0f379a1a3ce4d06efa37983b6d981
Ievgen Talerko <italerko@ecovadis.com>
2024-09-06 14:15:10 +0200

    updated

--- CustomWorkflow/Program.cs
+++ CustomWorkflow/Program.cs
@@ -7,6 +7,7 @@
             var inMemoryDatabase = new Dictionary<Guid, OrderState>();
             WorkflowWithFullPathExample(inMemoryDatabase);
             WorkflowWithSkippedStepsExample(inMemoryDatabase);
+            WorkflowWithRestoreExample(inMemoryDatabase);
         }
 
         void WorkflowWithFullPathExample(Dictionary<Guid, OrderState> inMemoryDatabase)
@@ -15,7 +16,6 @@
             workflow.Initialize();
 
             Console.WriteLine($"Current workflow ID is {workflow.WorkflowId}, current state is {workflow.GetCurrentState()}");
-            Console.WriteLine($"Current state is {workflow.GetCurrentState()}");
                 
             workflow.FireTrigger(OrderTrigger.StartProcessing);
             Console.WriteLine($"Current workflow ID is {workflow.WorkflowId}, current state is {workflow.GetCurrentState()}");
@@ -30,10 +30,30 @@
             workflow.Initialize();
 
             Console.WriteLine($"Current workflow ID is {workflow.WorkflowId}, current state is {workflow.GetCurrentState()}");
-            Console.WriteLine($"Current state is {workflow.GetCurrentState()}");
                 
             workflow.FireTrigger(OrderTrigger.SkipProcessing);
             Console.WriteLine($"Current workflow ID is {workflow.WorkflowId}, current state is {workflow.GetCurrentState()}");
+        }
+
+        /// <summary>
+        /// This example shows we can continue processing workflow at any moment afeter some pause, we need only workflowId to proceed
+        /// </summary>
+        /// <param name="inMemoryDatabase"></param>
+        void WorkflowWithRestoreExample(Dictionary<Guid, OrderState> inMemoryDatabase)
+        {
+            var workflow = new Workflow(inMemoryDatabase);
+            workflow.Initialize();
+
+            Console.WriteLine($"Current workflow ID is {workflow.WorkflowId}, current state is {workflow.GetCurrentState()}");
+                
+            workflow.FireTrigger(OrderTrigger.StartProcessing);
+            Console.WriteLine($"Current workflow ID is {workflow.WorkflowId}, current state is {workflow.GetCurrentState()}");
+
+            var workflowId = workflow.WorkflowId;
+
+            var restoredWorkflow = new Workflow(inMemoryDatabase, workflowId, inMemoryDatabase[workflowId]);
+            workflow.FireTrigger(OrderTrigger.FinishProcessing);
+            Console.WriteLine($"Current workflow ID is {restoredWorkflow.WorkflowId}, current state is {restoredWorkflow.GetCurrentState()}");
         }
     }
 }
